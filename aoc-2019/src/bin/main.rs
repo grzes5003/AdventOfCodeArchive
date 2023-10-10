10bb73e1dd9ab6389eb2be145d33b99bd6e1d192
grzes5003 <grzes.kulinski@gmail.com>
2023-10-10 23:37:33 +0200

    Solutions for Day08

--- aoc-2019/src/bin/main.rs
+++ aoc-2019/src/bin/main.rs
@@ -1,7 +1,7 @@
 #[allow(unused_imports)]
-use aoc_2019_impl::days::{Day01, Day02, Day03, Day04, Day06};
+use aoc_2019_impl::days::{Day01, Day02, Day03, Day04, Day06, Day08};
 use aoc_2019_impl::solve;
 
 fn main() {
-    println!("{:?}", solve::<Day06>());
+    println!("{:?}", solve::<Day08>());
 }
--- aoc-2019/src/days/mod.rs
+++ aoc-2019/src/days/mod.rs
@@ -3,9 +3,15 @@ mod day02;
 mod day03;
 mod day04;
 mod day06;
+mod day07;
+mod day08;
+mod day09;
 
 pub use day01::Day as Day01;
 pub use day02::Day as Day02;
 pub use day03::Day as Day03;
 pub use day04::Day as Day04;
 pub use day06::Day as Day06;
+pub use day07::Day as Day07;
+pub use day08::Day as Day08;
+pub use day09::Day as Day09;
